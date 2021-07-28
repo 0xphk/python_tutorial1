3b40a6ba79352f7dbca500abfa899efc8dbf3108
phk <phk@terminal21.de>
2021-07-28 14:57:12 +0200

    working user input for class method arguments

--- 13_methods.py
+++ 13_methods.py
@@ -12,35 +12,36 @@ class Label:
 
     # works as parameters are given
     def autobuy(self, amount, price):
-        self.owned += int(amount)
-        self.spent += int(price)
+        self.owned += amount
+        self.spent += price
 
     # does not work for user input
     def buy(self):
-        self.amount = None
-        self.price = None
+        self.amount = label1.amount
+        self.price = label1.price
+        self.owned += label1.amount
+        self.spent += label1.price
 
-    def checkout(self):
-        self.owned += int(amount)
-        self.spent += int(price)
+"""     def checkout(self):
+        self.owned += int(label1.amount)
+        self.spent += int(label1.price) """
 
 # print __doc__ object if class description is given
-print("Print class doc \'print(Label.__doc__)\'")
-print(Label.__doc__)
+print("\n[debug:] Print class doc \'print(Label.__doc__)\'")
+print("[debug:]",Label.__doc__,"\n")
 
 # set attributes for object
-label1 = Label("critical", "100+", "10", "100")
-label2 = Label("othercide","100+", "1", "10")
+print("set default values \'label1.name, label1.releases, label1.owned, label1.spent\'\n")
+label1 = Label("critical", "100+", "\t10", "\t100")
+label2 = Label("othercide","100+", "\t1", "\t10")
 
 # print dictionary object as json
-print("print given object as json")
-print("label1")
-print(label1.__dict__)
-print("label2")
-print(label2.__dict__)
+print("[debug:] print given object as json")
+print("[debug:] label1:",label1.__dict__)
+print("[debug:] label2:",label2.__dict__,"\n")
 
 # print some attrs
-print("\nLables - Releases:\n",label1.name, label1.releases, label1.owned,"\n",label2.name, label2.releases, label2.owned,"\n")
+print(" Label\t\t Releases\t Owned:\n\n",label1.name,"\t",label1.releases,"\t\t",label1.owned,"\n",label2.name,"\t",label2.releases,"\t\t",label2.owned,"\n")
 
 # print attrs before buy
 print("base values:")
@@ -48,19 +49,25 @@ print(label1.name,"owned:",label1.owned,"spent total:",label1.spent)
 print(label2.name,"owned:",label2.owned,"spent total:",label2.spent)
 
 # call function, change attributes
-#print("buy 1 critical for 10, buy 2 othercide for 36")
-#label1.autobuy(1,10)
-#label2.autobuy(2,36)
+print("autobuy 1 critical for 10, buy 2 othercide for 36\n")
+label1.autobuy(1,10)
+label2.autobuy(2,36)
 
+# use user input for method buy() arguments
+print("how much", label1.name, "bought?")
 label1.amount = int(input("amount: "))
 label1.price = int(input("price: "))
-# does not work, expecting parameters are taken from user input
-#print("how much to buy?")
+
 label1.buy()
-label1.checkout()
+#label1.checkout()
 
 # after buy
 print("values after increment buy parameter in buy() method")
 print(label1.name,"owned:",label1.owned,"spent total:",label1.spent)
 print(label2.name,"owned:",label2.owned,"spent total:",label2.spent)
+
+# print dictionary object as json
+print("print given object as json")
+print("label1:",label1.__dict__)
+print("label2:",label2.__dict__)
 
