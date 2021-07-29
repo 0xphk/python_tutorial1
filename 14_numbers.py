777624c964a81e3dc3d6d98705fe77e1f7050930
phk <phk@terminal21.de>
2021-07-29 15:02:53 +0200

    difference python2/3 in arithmetic and numbers

--- 14_numbers.py
+++ 14_numbers.py
@@ -1,21 +1,44 @@
-# assign value
+# python2+3 - int
 print("set a = 42")
 a = 42
+print("set b = 23")
+b = 23
+print("set c = 23.0 # set as float not int")
+c = 23.0
 
 # get type
-print("get type", type(a))
+print("get type a:", type(a), "\nget type b:", type(b), "\nget type c:", type(c))
 
 # print value of var - only works in plain python shell
 a
+b
+c
 
 # print value in script
-print("print(a):", a)
+print("print(a):", a, "\nprint(b):", b, "\nprint(c):", c)
+print()
 
-# python 2 max int value, python 3 has no attribute maxint
+# python2 - maxint
+# max int value, python 3 has no attribute maxint
 #import sys
 #sys.maxint()
 
-# use 'j' suffix to set copmplex numbers
+# python2 only ### use 'L' suffix for long numbers
+#print("set y = 1L")
+#y = 1L
+#print("get type:", type(y))
+
+# square / squareroot syntax
+print('''calculate square of n 'n²' using x = n ** 2
+>>> n = 5
+>>> x = n ** 2
+>>> x
+25''')
+
+# python2+3 - complex
+# use 'j' suffix to set complex numbers ('i' is 'j' in python) j = sqrt(-1+0j)
+# complex numbers have a 'real' and an 'imaginary' part
+# python uses floats for 'real' and 'imaginary numbers'
 print("set z = 3 + 5.7j")
 z = 3 + 5.7j
 
@@ -30,3 +53,67 @@ print("print(z.real):", z.real)
 
 # print 2nd real value? - confusing output
 print("print(z.imag):", z.imag)
+
+# python2+3 - float
+# floating point numbers
+print("set e = 2.718281828")
+e = 2.718281828
+
+# get type
+print("get type:", type(e))
+
+# print float
+print("print(e):", e)
+print()
+
+# python2
+# wide to narrow numbers
+# complex d > float c > long b > int a
+#a = 2
+#b = 3L
+#c = 6.0
+#d = 12 + 0j
+# Rule: widen numbers so they are the same type
+
+# python3
+# wide to narrow numbers
+# complex d > float c > int a
+a = 2
+c = 6.0
+d = 12 + 0j
+# Rule: widen numbers so they are the same type
+
+# arithmetic
+# python2 - addition int + long number
+# widen int first, convert int to long > then add > result is long number
+#a + b # int + long
+#print(a + b) #
+#5L
+
+print("# python3 - addition int + float")
+print(">>> int 2 > float 2.0 > add float 2.0 + float 6.0 = float 8.0")
+print("a:", a, type(a), "\nc:", c, type(c), "\nprint(a + c)\n_result:", a + c, type(a + c))
+print()
+
+print("# python3 - substraction complex - float")
+print(">>> float 6.0 > complex 6+0j > substract complex 12+0j - complex 6+0j = complex 6+0j")
+print("c:", c, type(c), "\nd:", d, type(d), "\nprint(d - c)\n_result:", d - c, type(d - c))
+print()
+
+print("# python3 - multiplication int * complex")
+print(">>> int 2 > complex 2+0j > multiply complex 2+0j * complex 12+0j = complex 24+0j")
+print("a:", a, type(a), "\nd:", d, type(d), "\nprint(a * d)\n_result:", a * d, type(a * d))
+print()
+
+print("# python3 - division complex / float")
+print(">>> float 6.0 > complex 6+0j > divide complex 12+0j / complex 6+0j = complex 2+0j")
+print("d:", d, type(d), "\nc:", c, type(c), "\nprint(d / c)\n_result:", d / c, type(d / c))
+print()
+
+print("# python2 - division int whole numbers\n'16/5' returns quotient w/o remainder: 3\n'16 % 5' returns remainder\n'16/float(5)' returns float")
+print()
+print("# python3 - division int whole numbers\n'16/5' returns correct float")
+a = 5
+b = 16
+print("a:", a, type(a), "\nb:", b, type(b), "\nprint(b / a)\n_result:", b / a, type(b / a))
+print()
