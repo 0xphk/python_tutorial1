2cdf667d2158b672eed3da7dab01e8d5e3d32fe0
phk <phk@terminal21.de>
2021-07-30 16:28:40 +0200

    formatting

--- 17_string_hex_conversion.py
+++ 17_string_hex_conversion.py
@@ -19,28 +19,29 @@ name_hex = name.encode('utf-8').hex()
 print("hex representation of string 'name' as hex string 'name_hex':", repr(name.encode('utf-8').hex()))
 print(">>> type of 'f{repr(name.encode('utf-8').hex()}':", type(repr(name.encode('utf-8').hex())))
 print("\nhex encoding of string 'name' as hex string 'name_hex':", name.encode('utf-8').hex())
+print(">>> better solution as it gives plain value without quotes")
 print(">>> type of 'f{name.encode('utf-8').hex()}':", type(name.encode('utf-8').hex()))
 
-# add new arguments as first and third positional argument
-print("\nput things together using '0x{2} {0}{1}{3}'.format('(', name, name_hex, ')')\n")
-print('hello 0x{2} {0}{1}{3}'.format('(', name, name_hex, ')'))
+# add new positional arguments
+print("\nput things together using '0x{2} {0}{1}{3}'.format('(', name, name_hex, ')')")
+print(">>> there are now 4 positional arguments for function format()\n")
+print('[hello 0x{2} {0}{1}{3}]'.format('(', name, name_hex, ')'))
 
-#print('hello 0x{2} {0} {1} {4}'.format("(", name, name_hex, ")"))
-
-# python3 can use bytearry to convert hex string back to ascii string
+# convert hex string back to string using decode(bytearray.fromhex(name_hex))
+print('\npython3 can use bytearray.fromhex(name_hex) to convert hex string back to ascii string')
 name_byte = bytearray.fromhex(name_hex)
-print("\nconvert hex value back from bytearray ascii string to string:\n")
-print('hello back {1} {0}0x{2}{3}'.format('(', name_byte.decode(), name_hex, ')'))
+print("\nconvert hex value back from bytearray ascii string 'name_byte' to string:\n")
+print('[hello back {1} {0}0x{2}{3}]'.format('(', name_byte.decode(), name_hex, ')'))
 print("\n>>> type of: 'name_byte.decode()':", type(name_byte.decode()))
 print()
 
-# python3 can also use module function codecs.decode() to convert hex string back to ascii string
-# use import codecs on top of file
-
+# convert hex string back to string using module 'codecs(decode(name_hex, 'hex'))'
+print('''python3 can also use function decode() in module 'codecs'
+>>> to convert hex string back to ascii string
+>>> to use import module using 'import codecs' on top of file\n''')
 name_bin = codecs.decode(name_hex, 'hex')
-#name_bin2 = name_bin.strip(decode(name_hex, 'hex'))
 print("convert hex value to binary_string:", name_bin)
 print(">>> type of 'f{codecs.decode(name_hex, 'hex')}':", type(codecs.decode(name_hex, 'hex')))
-print("\nconvert hex value back from binary_string to string:", str(name_bin, 'utf-8'))
+print("\nconvert hex value back from binary_string 'name_bin' to string:", str(name_bin, 'utf-8'))
 print(">>> type of '{str(name_bin, 'utf-8')}':", type(str(name_bin, 'utf-8')))
 print()
