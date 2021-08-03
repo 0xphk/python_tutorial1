31fb0a7b7987b7e21ae830ef208fda1ddf84d27d
phk <phk@terminal21.de>
2021-08-03 16:06:15 +0200

    import from file modules after renaming, cannot start w/ number

--- 21_dictionaries.py
+++ 21_dictionaries.py
@@ -1,2 +1,18 @@
+# import some functions and classes [doesn't work if filenames start w/ numbers eg. 98_test]
+# explicitly calling '__import__' builtin might help # strhex = __import__('98_test')
+from modules import strhex, now
+
+print('import strhex(), now() from file "modules"\nfrom modules import strhex, now\n')
+print('now():', type(now))
+print('strhex():', type(strhex))
+print(strhex.__doc__)
+
 # associative array or map = dictionary (json like key:value pairs) in python
-post = {"user_id":209, "message":"some message"}
+# use imported strhex function for message conversion to hex string
+post = {"user_id":209,"message":strhex('hex representated message')}
+
+# print(strhex('phk'))
+# print(strhex(str(post)))
+print(post)
+
+print('\nfinished:', now, '\n')
