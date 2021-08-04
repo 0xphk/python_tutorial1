8fd21d3720617b408cf1aeab60192137c09955e7
phk <phk@terminal21.de>
2021-08-04 14:35:55 +0200

    added custom time format

--- modules.py
+++ modules.py
@@ -9,5 +9,37 @@ def strhx(i):
 # print(strhx.__doc__)
 # print(strhx('phk'))
 
+def dtformat():
+    """format options for datetime objects"""
+    print('''\ncustom datetime formatting:
+
+    >>> time
+    >>> %Z - timezone name
+    >>> %z - UTC offset +HHMM or -HHMM
+    >>> %p - locales equivalent am or pm
+    >>> %I - 12h hour as zero padded decimal number
+    >>> %H - 24h hour as zero padded decimal number
+    >>> %M - minute as zero padded decimal number
+    >>> %S - second as zero padded decimal number
+    >>> %f - microsecond as zero padded decimal number
+
+    >>> day, weekday
+    >>> %d - day of month as zero padded decimal number
+    >>> %j - day of the year as as zero padded decimal number
+    >>> %w - weekday as decimal number 0=Sunday 6=Saturday
+    >>> %a - weekday abbreviated name
+    >>> %A - weekday full name
+
+    >>> year
+    >>> %Y - year w/ century as decimal number
+    >>> %y - year w/o century as zero padded decimal number
+
+    >>> month
+    >>> %B - months full name
+    >>> %b - months abbreviated name
+    >>> %m - month as zero padded decimal number\n''')
+
+format = "%Y%m%dT%H%M%SZ"
 now = datetime.datetime.today()
+nowf = now.strftime(format)
 # print('script finished', now, '\n')
