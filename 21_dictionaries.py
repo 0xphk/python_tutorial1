0376a365c00c388ab3d5ae0dd483028c1ab2a287
phk <phk@terminal21.de>
2021-08-04 17:55:57 +0200

    cleanup

--- 21_dictionaries.py
+++ 21_dictionaries.py
@@ -11,8 +11,7 @@ print('>>> strhx.__doc__: ',strhx.__doc__,sep='')
 # associative array or map = dictionary (json like key:value pairs) in python
 
 post_plain = {'user_id':209,'message':'hex representated message','language':'English','datetime':'20210804T134904Z','location':(44.590533, -104.715556)}
-post_hex = {'user_id':209,'message':strhx('hex representated message'),'language':'English','datetime':'20210804T134904Z','location':(44.590533, -104.715556)}
-all_hex = strhx(str(post_hex))
+post_hex = {'user_id':209,'message':strhx(str(post_plain['message'])),'language':'English','datetime':'20210804T134904Z','location':(44.590533, -104.715556)}
 
 print('''\n>>> display items(key:value pair) in dictionary
 print(post_plain.items())\n''', post_plain.items(), sep='\n')
@@ -31,62 +30,67 @@ post_plain_index = list(post_plain.keys())
 print('''\n>>> indirectly access dict items using index[1] but requires to create an additional list
 >>> index is only available using this list, not the plain dict !
 post_plain_index = list(post_plain.keys())''', sep='\n')
-print(post_plain.keys(), '\nprint(post_plain_index[0]): ', post_plain_index[0], sep='')
+print('\n', post_plain.keys(), '\nprint(post_plain_index[0]): ', post_plain_index[0], sep='')
 
 # create hash objects for user_id value
 print('''\n>>> create utf-8 encoded hash object 'post_plain_hash' for user_id
->>> post_plain_hash = hashlib.sha256(str(post_plain['user_id']).encode('utf-8'))''')
+post_plain_hash = hashlib.sha256(str(post_plain['user_id']).encode('utf-8'))''')
 post_plain_hash = hashlib.sha256(str(post_plain['user_id']).encode('utf-8'))
-print('post_plain_hash: ', post_plain_hash, 'type: ', type(post_plain_hash))
+print('\npost_plain_hash: ', post_plain_hash, '\ntype: ', type(post_plain_hash))
 
 # create digest / hex representation of hash object
 print('''\n>>> create hex representation of hash object 'post_plain_hash'
->>> post_plain_digest = post_plain_hash.hexdigest()''')
+post_plain_digest = post_plain_hash.hexdigest()''')
 post_plain_digest = post_plain_hash.hexdigest()
-print('post_plain_digest: ', post_plain_digest, '\ntype: ', type(post_plain_digest))
+print('\npost_plain_digest: ', post_plain_digest, '\ntype: ', type(post_plain_digest))
 
 # add additional key:value pairs to dict
 print('''\n>>> add new key 'hash' with sha256 value of 'user_id' (post_plain_digest) to dict post_plain''')
 print('''post_plain['hash'] = post_plain_digest''')
 post_plain['hash'] = post_plain_digest
-print(post_plain)
+print('\n',post_plain.keys())
 
 # update value for key in dict from dict2 post_plain_update w/ same key
 print('''\n>>> update value in post_plain['location'] using post_plain.update() method and
 >>> 2nd dictionary 'post_plain_update' as input''')
 post_plain_update = {'location':(94.590533, -24.715556)}
-print('''dict2: post_plain_update = {'location':(94.590533, -24.715556)}''')
-print('''post_plain key ['location']:''', post_plain['location'])
-print('''post_plain_update key ['location']:''', post_plain_update['location'])
-print('\npost_plain.update(post_plain_update), post_hex.update(post_plain_update)')
+print('''\ndict1:\npost_plain key ['location']:''', post_plain['location'])
+print('''\ndict2:\npost_plain_update key ['location']:''', post_plain_update['location'])
+print('\nupdate keys in both dictionaries\npost_plain.update(post_plain_update), post_hex.update(post_plain_update)')
 post_plain.update(post_plain_update)
 post_hex.update(post_plain_update)
-print('''\npost_plain, post_hex updated keys ['location']:''', post_plain['location'], post_hex['location'], '\nnow both match post_plain_update', sep='\n')
+print('''\n>>> post_plain, post_hex updated keys ['location']:
+post_plain['location']: ''', post_plain['location'], '''\n
+post_hex['location']: ''', post_hex['location'], '\n\nnow both match post_plain_update', sep='')
 
 # hex conversion / compare
 print('\nuse imported strhx function for message conversion to hex string')
-print('\npost_plain:',post_plain,sep='\n')
-print(type(post_plain))
-print('\npost_hex:',post_hex,sep='\n')
-print(type(post_hex))
-# print('\nall_hex (whole dictionary):',all_hex,sep='\n')
-# print(type(all_hex))
+print('\npost_plain:\n', post_plain['message'], type(post_plain['message']),sep='\n')
+print('\npost_hex:\n', post_hex['message'], type(post_hex['message']),sep='\n')
+
+# note, no hash in post_hex dict, update does not create keys
+print('\nNOTE: no hash key in post_hex! we only added it to post_plain\n\npost_plain:', post_plain, '\npost_hex:', post_hex, sep='\n')
 
 # arbitrary value for compare test
-hx = '7b27757365725f6964273a203230392c20276d657373616765273a20273638363537383230373236353730373236353733363536653734363137343635363432303664363537333733363136373635277d'
-hx_db = '68657820726570726573656e7461746564206d657373616765'
+print('\n>>> compare hex values w/ db')
+hx = '7b27757365725f6964273a203230392c20276d657373616765273a20273638363537383230373236353730373236353733363536653734363137343635363432303664363537333733363136373635272c20276c616e6775616765273a2027456e676c697368272c20276461746574696d65273a20273230323130383034543133343930345a272c20276c6f636174696f6e273a202839342e3539303533332c202d32342e373135353536297d'
+hx_msg = '68657820726570726573656e7461746564206d657373616765'
+hx_all = strhx(str(post_hex))
 
-print('\nhx_db:',hx_db)
-print('\npost_hex.get(\'message\'):',post_hex.get('message'))
+# print('\nhx_all (whole dictionary):',hx_all,sep='\n')
+# print(type(hx_all))
+print('\nhx_msg:',hx_msg)
+print('\npost_hex[\'message\']:',post_hex['message'])
 
-if post_hex.get('message') == hx_db:
+if post_hex['message'] == hx_msg:
     print('\n>>> VALUE MATCH')
 else:
     print('\n>>> VALUE NO_MATCH')
 
-if all_hex == hx:
-    print('\ninput:',all_hex,'\ndb:',hx,'\n>>> MATCH',sep='\n')
+print('\n>>> compare hex for complete dict')
+if hx_all == hx:
+    print('\ninput:',hx_all,'\nmsg:',hx,'\n>>> MATCH',sep='\n')
 else:
-    print('\ninput:',all_hex,'\ndb:',hx,'\n>>> NO_MATCH',sep='\n')
+    print('\ninput:',hx_all,'\nmsg:',hx,'\n>>> NO_MATCH',sep='\n')
 
 print('\nfinished:', now, nowf, '\n')
