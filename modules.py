0b7453c75cf299863a9871af4cb9f6731ed8fb72
phk <phk@terminal21.de>
2021-08-05 16:17:42 +0200

    added class colors to modules

--- modules.py
+++ modules.py
@@ -49,4 +49,18 @@ def nowf():
     format = "%Y%m%dT%H%M%SZ"
     return now().strftime(format)
 
-# print('\nfinished:', now(), nowf(), '\n')
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    # 256color forground [38;5;$idm]
+    DARKYELLOW = '\033[38;5;136m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+# print(color.DARKYELLOW, '\nfinished:', color.BOLD, color.GREEN, now(), color.CYAN, nowf(), color.END, '\n')
