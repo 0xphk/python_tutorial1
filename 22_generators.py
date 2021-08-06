7cb594ecaa08ded142b28f2b6d6ea8265c8ce12c
phk <phk@terminal21.de>
2021-08-06 16:23:00 +0200

    added 22_generators.py w/ generator example count char

---
+++ 22_generators.py
@@ -0,0 +1,32 @@
+# converts function in generator using 'yield' keyword
+from modules import spinit, term_reset
+
+# function to match character 'e' in string
+def printresult(String):
+    for i in String:
+        if i == "e":
+            yield i
+
+# initializing string
+String = "GeeksforGeeks"
+
+# var init value, used for calculate count of "e" in 'String'
+ans = 0
+
+# reset term
+term_reset()
+
+# fprint status line, no newline
+print(f"The number of 'e' in word {String} == ",end="")
+
+# spinning cursor w/ custom count
+spinit(6)
+
+# strip does nothing here
+# String = String.strip()
+
+# increment 'ans' w/ 'yield' return values
+for j in printresult(String):
+    ans = ans + 1
+
+print(ans)
