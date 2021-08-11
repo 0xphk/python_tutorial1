57520f7a8e57185276547df0b8be77f60f7a4253
phk <phk@terminal21.de>
2021-08-11 16:05:04 +0200

    added 23_tuples.py

---
+++ 23_tuples.py
@@ -0,0 +1,9 @@
+# python3 tuples: can be considered as immutable/hashable list
+
+"""
+Python3: List vs Tuple vs Set vs Dictionary
+Lists - for ordered sequence of indexed objects
+Tuple - can be considered as immutable/hashable list
+Python Set - unique list
+Python Dictionary / dict - pair of key and values
+"""
