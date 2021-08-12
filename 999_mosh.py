c5df1ddc09ba7c0385e9ab6f28497aa4f40ac08e
phk <phk@terminal21.de>
2021-08-12 14:32:31 +0200

    added modules.term_reset(), modules.color

--- 999_mosh.py
+++ 999_mosh.py
@@ -1,4 +1,6 @@
 # mosh [https://www.youtube.com/watch?v=kqtD5dpn9C8]
+from modules import color, term_reset, spinit
+term_reset()
 
 # vars ~5m - ~9m
 name = " John Smith"
@@ -19,17 +21,31 @@ age = 2021 - int(birth_year)
 if int(birth_month) < int(current_month):
     age += 1
 print('you are currently: ',str(age).strip(),'years old!')
+print('reset term in 3s ...')
+print(color.DARKYELLOW)
+spinit(10)
+print(color.END)
+term_reset()
 
+# calculate w/ different datatypes
 First = 10.1
 Second = 20
 Sum = First + float(Second)
 print('First:\t',First,'\t',type(First),'\nSecond:\t',Second,'\t',type(Second),'\nSum:\t',Sum,'\t',type(Sum))
+print('reset term in 3s ...')
+print(color.DARKYELLOW)
+spinit(10)
+print(color.END)
+term_reset()
 
 # strings ~19m - 
 course = "Python for Beginners"
-print(course)
-print('using str.upper() method:',course.upper())
+print('course:',color.CYAN,course,color.END,end='\n')
+print('using str.upper() method:',color.DARKCYAN,color.BOLD,course.upper(),color.END)
 
 # find index of letter 'y'
 print('find index of letter "y":',course.find('y'))
 print('replace lowercase "ython" to uppercase:',course.replace('ython', 'YTHON'))
+
+# check for parts of str(), return bool
+print('is "Python" part of str(course):','Python' in course)
