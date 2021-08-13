273fefd62aa7073920a7fc3c4b7fa22f4ece32e7
phk <phk@terminal21.de>
2021-08-13 13:54:24 +0200

    added class ansicolors, color all the things!

--- modules.py
+++ modules.py
@@ -92,5 +92,43 @@ class color:
     DARKYELLOW = '\033[38;5;136m'
     # non color ansi codes
     CLS = '\033[2J'
-
 # print(color.DARKYELLOW, '\nfinished:', color.BOLD, color.GREEN, now(), color.CYAN, nowf(), color.END, '\n')
+
+class ansicolor:
+    reset = '\033[0m'
+    bold = '\033[01m'
+    disable = '\033[02m'
+    underline = '\033[04m'
+    reverse = '\033[07m'
+    strikethrough = '\033[09m'
+    invisible = '\033[08m'
+    cls = '\033[2J'
+    class fg:
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m'
+        darkcyan = '\033[36m'
+        darkgray = '\033[38;5;240m'
+        darkyellow = '\033[38;5;136m'
+    class bg:
+        black = '\033[40m'
+        red = '\033[41m'
+        green = '\033[42m'
+        orange = '\033[43m'
+        blue = '\033[44m'
+        purple = '\033[45m'
+        cyan = '\033[46m'
+        lightgrey = '\033[47m'
+
