2fa0392cb2e7132417c908d03514a28089e63d82
phk <phk@terminal21.de>
2021-08-13 15:49:41 +0200

    added naming convention for variables

--- 04_variables.py
+++ 04_variables.py
@@ -12,3 +12,13 @@ result = eval('9 + 2')
 var1 = 22
 var2 = 22 + 3
 var3 = (22 - 2) * 3
+
+# naming conventions
+name = "James"
+# underscore preferred over camelCase
+full_name = "John James"
+# camelCase
+fullName = "John James"
+# uppercase for constants / fixed values
+PI = 3.14159
+E = 2.1718281828
