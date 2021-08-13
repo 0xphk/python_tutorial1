67d6c34092d91159fe98a3986ca0c90b97af8511
phk <phk@terminal21.de>
2021-08-13 16:05:12 +0200

    explicitly define datatype for var

--- 04_variables.py
+++ 04_variables.py
@@ -1,9 +1,9 @@
 # variables
 var1 = 1
-var2 = input("enter 2nd input: ")
+#var2 = input("enter 2nd input: ")
 
 # var as specific input data type
-var3 = int(input("enter 3rd input: "))
+#var3 = int(input("enter 3rd input: "))
 
 # store results
 result = eval('9 + 2')
@@ -22,3 +22,17 @@ fullName = "John James"
 # uppercase for constants / fixed values
 PI = 3.14159
 E = 2.1718281828
+
+# explicitly define datatype for variable
+name: str = "John"
+isAdult: bool = True
+my_list: list = [1, 2, 3]
+decimal: float = 3.14
+my_tuple: tuple = (1, 2, 3)
+also_a_tuple: tuple = 1, 2, 3.14, '4'
+print(type(name))
+print(type(isAdult))
+print(type(my_list))
+print(type(decimal))
+print(type(my_tuple))
+print(type(also_a_tuple))
