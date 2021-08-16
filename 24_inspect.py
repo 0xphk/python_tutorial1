5c4b506304cbbcadaabe95b4e2e7bb6e8b5aa2b0
phk <phk@terminal21.de>
2021-08-16 14:59:41 +0200

    inspect errors for builtins written in C (cpython.so)

--- 24_inspect.py
+++ 24_inspect.py
@@ -1,5 +1,6 @@
 # inspect source of module if lang = python
 from modules import color
+from math import isqrt
 import inspect
 
 # print color class source
@@ -7,4 +8,11 @@ print(color.BOLD,color.DARKCYAN,"inspect sources of imported modules, own class:
 try:
     print(inspect.getsource(color))
 except OSError:
-    print("src not available, not a python module?")
+    print("src not available, or not a python module?")
+
+# won't work for builtins as most of them a pure C, look at srcs instead
+try:
+    # print(inspect.getsourcelines(isqrt))
+    print('if this returns cpython, it is written in C',inspect.getmodule(isqrt))
+except OSError:
+    print("src not available, or not a python module?")
