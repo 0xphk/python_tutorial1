3b0daba8d9732d0ac861ec169d3a6ee8108e755d
phk <phk@terminal21.de>
2021-08-16 16:06:22 +0200

    Calc class for import tests

--- modules.py
+++ modules.py
@@ -141,3 +141,26 @@ class ansicolor:
         purple = '\033[45m'
         cyan = '\033[46m'
         lightgrey = '\033[47m'
+
+class Calc:
+    # def __init__(self):
+    def add(n1,n2):
+        return n1 + n2
+
+    def sub(n1,n2):
+        return n1 - n2
+
+    def mul(n1,n2):
+        return n1 * n2
+
+    def div(n1,n2):
+        return n1 / n2
+
+    def idiv(n1,n2):
+        return n1 // n2
+
+    def rdiv(n1,n2):
+        return n1 % n2
+
+    def exp(n1,n2):
+        return n1 ** n2
