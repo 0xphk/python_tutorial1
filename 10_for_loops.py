8c3e31781c31689f773dbb342da75931358fe833
phk <phk@terminal21.de>
2021-08-16 13:59:41 +0200

    for loop addition over list numbers

--- 10_for_loops.py
+++ 10_for_loops.py
@@ -12,3 +12,19 @@ for int in range(0, 10):
 print("# use range(1,10,2) start stop step")
 for int in range(1, 10, 2):
     print(int)
+
+# loop through dicts
+person = {'name':'Jamal','age':18,'state':'US'}
+
+# combined for loop condition key:value
+print(person.items())
+for key, value in person.items():
+    print(f'key:{key} value:{value}')
+
+# addition over all numbers in list
+numbers = [1,3,5,6,7,9]
+result = 0
+for number in numbers:
+    result += number
+
+print(result)
