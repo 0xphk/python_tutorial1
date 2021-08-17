b2c24c1f79bf6270a56a34089848edc12f9d7c05
phk <phk@terminal21.de>
2021-08-17 11:40:11 +0200

    fill list w/ random stuff

--- 95_randomlist.py
+++ 95_randomlist.py
@@ -1,9 +1,10 @@
+from modules import strhx
 import random
-import base64
 
 rlist = []
-
-print()
+rot13 = bytes.maketrans(
+    b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    b"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM")
 
 count = 0
 while count < 100:
@@ -13,20 +14,13 @@ while count < 100:
     rhex2 = random.randbytes(4).hex()
     rstr = str(rhex1 + str(rint) + rhex2)
     rlist.append(rstr)
-rbhead = base64.b64encode(b'total_random_stuff')
-# replace().replace() works but is ugly
-rhead = str(rbhead).replace("b","",1).replace("'","")
-print(rhead)
-print('rhead:',rhead,type(rhead))
+rhead = 'totally_random_stuff'
+rthead = strhx(rhead.translate(rot13))
+rlist.insert(0,rthead)
 
-rlist.insert(0,rhead)
-
-# do not understand this yet
-# removeSpecialChars = z.translate ({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"})
-# removeSpecialChars = rhead.translate ({ord(c): " " for c in "'b"})
+print('rhead:',rhead,len(rhead),type(rhead))
+print('rthead:',rthead,len(rthead),type(rthead))
 
 # verify str.len(), rand_list.__len__()
 print('rstr.len():',len(rstr),'\nrand_list.__len__():',rlist.__len__())
 print('list:',rlist,sep='\n')
-
-# print(rpl('12764513764'))
