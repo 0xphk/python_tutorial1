5a59356fe2f3d901540b52fa1fec52864d2ef30d
phk <phk@terminal21.de>
2021-09-08 15:04:32 +0200

    simple calc for import

---
+++ calc.py
@@ -0,0 +1,21 @@
+# simple calc for import test
+def add(n1,n2):
+    return n1 + n2
+
+def sub(n1,n2):
+    return n1 - n2
+
+def mul(n1,n2):
+    return n1 * n2
+
+def div(n1,n2):
+    return n1 / n2
+
+def idiv(n1,n2):
+    return n1 // n2
+
+def rdiv(n1,n2):
+    return n1 % n2
+
+def exp(n1,n2):
+    return n1 ** n2
