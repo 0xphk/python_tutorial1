96df7f1dea9b775c84d8d924c29c4bb992be0695
phk <phk@terminal21.de>
2021-09-09 21:06:27 +0200

    cleanup, description

--- 104_collatz_sequence.py
+++ 104_collatz_sequence.py
@@ -1,3 +1,14 @@
+""" personal notes:
+
+    # should run it but it doesn't
+    collatz(user)
+
+    # this actually runs the function but i have not f*cking idea why
+    list(collatz(user))
+
+    # runs function and prints list 
+    print(list(collatz(user)))
+"""
 import sys
 from modules import treset
 
@@ -8,13 +19,14 @@ print('computes collatz seq for given number\n')
 def collatz(number):
     while number != 1:
         if number % 2 == 0:
-            # number = number // 2 or in short
-            number //= 2
+            number //= 2  # number = number // 2 in short
             print(number, end=' ')
-            yield number
-            # requires yield - makes a generator out of this function
-            # some sort of updating value and return it to function?
-            # instead of return which seems to exit function call
+            yield number  # requires yield instead of return
+            """ personal notes:
+                - yield statement makes a generator out of this function
+                - some sort of updating value and return it back to function ?
+                - return statement seems to exit function call instead of returning value back to function
+            """
         else:
             number = number * 3 + 1
             print(number, end=' ')
@@ -28,23 +40,13 @@ try:
                 print('no negative feelings please ...')
                 continue
             print('collatz seq: ', end='')
-            # this actually runs the collatz() function and i have not f*cking idea why
-            list(collatz(user))
+            list(collatz(user))  # this calls collatz() no f*cking idea why collatz(user) doesn't
             print()
             break
+
         except ValueError:  # int() input validation
             print('not a number')
+
 except KeyboardInterrupt:
     print()
     sys.exit()
-
-# should run it but it doesn't
-#collatz(user)
-
-# this actually runs the function but i have not f*cking idea why
-#list(collatz(user))
-
-# runs function and prints list as well
-#print(list(collatz(user)))
-
-#print(end='\n\n')
