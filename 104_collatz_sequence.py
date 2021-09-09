857f153cf45b89793667cf970ce03b019c726524
phk <phk@terminal21.de>
2021-09-09 18:27:03 +0200

    collatz changes

--- 104_collatz_sequence.py
+++ 104_collatz_sequence.py
@@ -4,21 +4,35 @@ def collatz(number):
     while number != 1:
         if number % 2 == 0:
             number = number // 2
-            print(number)
+            print(number, end='')
+            # requires yield (some sort of updating value and return it to function?)
+            # instead of return which seems to exit function call
             yield number
+            #return number
         else:
             number = number * 3 + 1
-            print(number)
+            print(number, end='')
             yield number
+            #return number
 
 try:
-    while True:
-        try:
-            user = int(input('number: '))
-            print(list(collatz(user)))
-        # int() input validation
-        except ValueError:
-            print('enter an integer!')
+    try:
+        user = int(input('number: '))
+
+    # int() input validation
+    except ValueError:
+        print('enter an integer!')
+
 except KeyboardInterrupt:
     print('',end='\n')
     sys.exit()
+
+# should run it but it doesn't
+# collatz(user)
+
+# works but i have no f*cking idea why
+list(collatz(user))
+
+# calling it this way seems to be the only way to get this work
+# this expression runs it to create list
+#print(list(collatz(user)))
