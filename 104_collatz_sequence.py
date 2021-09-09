adf9df32f8b8fd33234d84682f4797cb14d7f87f
phk <phk@terminal21.de>
2021-09-09 20:23:35 +0200

    exceptions

--- 104_collatz_sequence.py
+++ 104_collatz_sequence.py
@@ -1,4 +1,7 @@
+# does not work yet
 import sys
+
+print()
 
 def collatz(number):
     while number != 1:
@@ -6,34 +9,67 @@ def collatz(number):
             #number = number // 2
             # same as above but shorter
             number //= 2
-            print(number, end='')
+            print(number, end=' ')
+            yield number
             # requires yield - makes a generator out of this function
             # some sort of updating value and return it to function?
             # instead of return which seems to exit function call
-            yield number
         else:
             number = number * 3 + 1
-            print(number, end='')
+            print(number, end=' ')
             yield number
 
 try:
     try:
-        user = int(input('number: '))
+        while True:
+            user = int(input('enter number: '))
+
+            if user < 0:
+                print('no negative feelings please ...')
+                break
+
+            print('collatz seq: ', end='')
+            list(collatz(user))
+            print()
+            break
 
     # int() input validation
     except ValueError:
-        print('enter an integer!')
+        print('not a number')
 
 except KeyboardInterrupt:
-    print('',end='\n')
+    print()
     sys.exit()
+
+#try:
+#    while True:
+#        try:
+#            user = int(input('enter integer: '))
+#            
+#            if user < 0:
+#                print('only postive numbers')
+#            continue
+#
+#            list(collatz(user))
+#            print(list(collatz(user)))
+#        # int() input validation
+#        except ValueError:
+#            print('enter a number')
+#except KeyboardInterrupt:
+#    print('',end='\n')
+#    sys.exit()
 
 # should run it but it doesn't
 # collatz(user)
 
 # works but i have no f*cking idea why
-list(collatz(user))
+#print('collatz seq: ', end='')
+#try:
+#    list(collatz(user))
+#except:
+#    print('did not work, try using integer')
 
-# calling it this way seems to be the only way to get this work
-# this expression runs it to create list
+# runs function and prints list as well
 #print(list(collatz(user)))
+
+#print(end='\n\n')
