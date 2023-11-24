41dcafc7a8ba0fff2279f3e24711339e7ceb0bad
phk <phk@terminal21.de>
2023-11-24 22:32:41 +0100

    cleanup for comparison

--- 27_arguments_base64.py
+++ 27_arguments_base64.py
@@ -9,23 +9,47 @@ class ansi:
   BD = '\x1b[1m'
   NM = '\x1b[0m'
 
-usr = 'ham-csi_prd'
+usr = '{{ auth_user }}'
 b64_str = 'c29tZV9lbmNvZGVfdGV4dAo='
 
 # check if argument is passed so len == 2 (arg0 == script, arg1 == second argument but first usable sys.argv[1])
+#if len(sys.argv) == 2:
+#  print('>>> sys.argv[0] is the name of the script, like in bash "${0}", script_name: ', ansi.GB, sys.argv[0], ansi.NM)
+#  b64_arg = sys.argv[1]
+#  print(f'>>> number of arguments: ', ansi.GB, len(sys.argv), ansi.NM, sep='', end='\n')
+#  print(f'>>> current arguments: ', ansi.GB, sys.argv, ansi.NM, sep='', end='\n')
+#  def decode_b64_arg(*args):
+#    decarg_bytes = base64.b64decode(b64_arg)
+#    return decarg_bytes.decode("utf-8")
+#  print(f'>>> base64_argument.decoded: ' + ansi.GB + decode_b64_arg(sys.argv[1]) + ansi.NM, sep='', end='\n')
+#else:
+#  def decode_b64(n):
+#    dec_b64_bytes = base64.b64decode(b64_str)
+#    return dec_b64_bytes.decode("utf-8")
+#  print('>>> sys.argv condition not matching, ' + ansi.RB + 'use hardcoded value!' + ansi.NM)
+#  print(f'>>> base64_var.decoded: ', ansi.GB, decode_b64(b64_str), ansi.NM, sep='', end='\n')
+
+# w/o comments
 if len(sys.argv) == 2:
   b64_arg = sys.argv[1]
+<<<<<<< Updated upstream
   print(f'>>> sys.argv[0] is the name of the script, like in bash "${0}", script_name: ', ansi.GB, sys.argv[0], ansi.NM, sep='', end='\n')
   print(f'>>> number of arguments: ', ansi.GB, len(sys.argv), ansi.NM, sep='', end='\n')
   print(f'>>> current arguments: ', ansi.GB, sys.argv, ansi.NM, sep='', end='\n')
+=======
+>>>>>>> Stashed changes
   def decode_b64_arg(*args):
     decarg_bytes = base64.b64decode(b64_arg)
     return decarg_bytes.decode("utf-8")
-  print(f'>>> base64_argument.decoded: ' + ansi.GB + decode_b64_arg(sys.argv[1]) + ansi.NM, sep='', end='\n')
+  print(f"\n"'>>> base64_argument.decoded: ' + ansi.GB + decode_b64_arg(sys.argv[1]) + ansi.NM, sep='', end='\n')
 else:
   def decode_b64(n):
     dec_b64_bytes = base64.b64decode(b64_str)
     return dec_b64_bytes.decode("utf-8")
+<<<<<<< Updated upstream
   print(f'>>> sys.argv condition not matching, ', ansi.RB, 'use hardcoded value!', ansi.NM, sep='', end='\n')
   print(f'>>> base64_var.decoded: ', ansi.GB, decode_b64(b64_str), ansi.NM, sep='', end='\n')
 
+=======
+  print(f"\n"'>>> base64_var.decoded: ', ansi.GB, decode_b64(b64_str), ansi.NM, sep='', end='\n')
+>>>>>>> Stashed changes
